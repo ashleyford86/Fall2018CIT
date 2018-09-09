9258b40f82d14a581cc6d43636aaebd0efcf59fb
ashleyford86 <ashleymote86@gmail.com>
2018-09-09 17:38:48 -0400

    a1

---
+++ a1/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/asmote/htdocs/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:23631/a1/Debug-Exercise-master/');
+
+//added a closing php tag and added the right path/url root
+?>
