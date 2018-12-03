d78dba953ff69d6527c51a0d27fd632da88e10c2
ashleyford86 <ashleymote86@gmail.com>
2018-12-02 21:10:27 -0500

    here

--- final/views/elements/footer.php
+++ final/views/elements/footer.php
@@ -1,6 +1,4 @@
-   <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+
     <script src="<?php echo BASE_URL?>views/js/jquery.js"></script>
     <script src="<?php echo BASE_URL?>views/js/bootstrap.min.js"></script>
    <?php
